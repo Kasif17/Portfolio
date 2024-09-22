08c5b32ddfe3abb942381a653d25985c19201c8d
Kasif17 <developer786kasif@gmail.com>
2024-09-22 21:03:16 +0530

    fisrtAndLast

---
+++ src/content_option.js
@@ -0,0 +1,140 @@
+const logotext = "Mohd Kasif Khan";
+const meta = {
+    title: "Mohd Kasif Khan",
+    description: "Full-stack MERN developer specializing in backend development with Node.js. Expert in building robust, scalable applications using modern technologies",
+};
+
+const introdata = {
+    title: "I’m Mohd Kasif Khan",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop Web Application",
+    },
+    description: "Full-stack MERN developer specializing in backend development with Node.js. Expert in building robust, scalable applications using modern technologies",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "Mohd Kasif Khan is a full-stack MERN developer specializing in backend development with Node.js, and has strong expertise in data structures and algorithms (DSA). Passionate about building scalable, robust applications.",
+};
+
+
+const skills = [{
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "Oops",
+        value: 95,
+    },
+    {
+        name: "HTML",
+        value: 95,
+    },
+    {
+        name: "CSS",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Node.js",
+        value: 90,
+    },
+    {
+        name: "ExpressJs",
+        value: 90,
+    },
+    {
+        name: "React.Js",
+        value: 70,
+    },
+    {
+        name: "MongoDB",
+        value: 85,
+    },
+    {
+        name: "MySQL",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://media.licdn.com/dms/image/C4D12AQE7p2OstXBmpw/article-cover_image-shrink_600_2000/0/1619277092995?e=2147483647&v=beta&t=uFgC096HTFbP0wbdR1XpcZNOPMN3ZOcuqa1BsT7zYYM",
+        description: "Developed a food ordering system using Node.js and Express.js. Implemented RESTful API routes, with MongoDB for data storage. Secured user authentication using bcrypt and JWT. Frontend built with React, React Router for navigation, and Bootstrap for a responsive UI.",
+        link: "https://github.com/Kasif17/foodOrder-Application"
+    },
+
+
+    {
+        img: "https://i.ytimg.com/vi/W-Oqe8Ph_eM/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBQIP_IkaCMiW0vNbBOmjp3G8qghQ",
+        description: "Developed a food recipe application using React.js, featuring data fetching with Axios from the themealdb API. The app provides users with a variety of recipes, allowing easy navigation and search functionality. Designed a clean, responsive UI with CSS and Bootstrap to enhance user experience.",
+        link: "https://food-recipes-application.netlify.app/",
+    },
+    {
+        img: "https://www.shutterstock.com/shutterstock/photos/1025080504/display_1500/stock-photo-conceptual-business-illustration-with-the-words-issue-tracking-system-1025080504.jpg",
+        description: "Developed an expense tracker using React.js and Bootstrap to help users manage their finances effectively. Leveraged useContext and useReducer hooks for state management, ensuring a smooth and dynamic user experience. Integrated Chart.js to visualize expenses, providing users with clear insights into their spending patterns.",
+        link: "https://github.com/Kasif17/IssueTracker_",
+    },
+    {
+        img: "https://user-images.githubusercontent.com/79099734/156505537-8e28ee14-dd20-4299-9eea-984d7068c7fd.png",
+        description: "Developed a Netflix clone using Node.js for the backend and React.js for the frontend. Implemented user authentication, video streaming functionality, and responsive design for a seamless user experience.",
+        link: "https://github.com/Kasif17/Netflix-App",
+    },
+    {
+        img: "https://5.imimg.com/data5/SELLER/Default/2022/9/RN/SY/GL/96727933/40-hotel-booking-500x500.jpg",
+        description: "Developed a booking reservation system using Node.js and React.js. Integrated RESTful APIs for real-time availability, user authentication, and payment processing. Designed a responsive interface for smooth user interaction across devices.",
+        link: "https://github.com/Kasif17/Booking-Reservation",
+    },
+
+   
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "itskasif3@gmail.com",
+    YOUR_FONE: "+91-9794975553",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_9bqxgzb",
+    YOUR_TEMPLATE_ID: "template_2i3qv6b",
+    YOUR_USER_ID: "Cxe3gx1xWoxoEwxOj",
+};
+
+const socialprofils = {
+    github: "https://github.com/Kasif17",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/mohd-kasif-khan-7704a0212/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
